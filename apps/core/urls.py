2f6f8ad284edffb2af4bf64c493c0890c9e6db97
Zachary Lake <zlake23@gmail.com>
2020-04-26 11:50:35 -0700

    Updates url paths in core app

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -4,5 +4,7 @@ from apps.core import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
+    path('create-event/', views.create_event, name='create_event'),
+    path('edit-event/<event_id>/', views.edit_event, name='edit_event'),
+    path('event/<event_id>/', views.event, name='event'),
 ]
